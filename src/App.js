3c387c736e9640993ba05082608201e0afc7e1ae
Etienne Jeanny <etienne.jeanny@gmail.com>
2020-04-08 12:22:44 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.scss";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from "./components/TourList";
+
+export default class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar></Navbar>
+                <TourList></TourList>
+            </React.Fragment>
+        );
+    }
+}
